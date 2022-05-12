e48ee6b721a4b172ab62d0e1aafe10691a2b1344
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2022-05-12 20:36:06 +0200

    -

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 
-import time
+import datetime
 import random
 import sys
+import os
+
+try:
+    import requests
+except ImportError:
+    raise ImportError("The Requests library (https://docs.python-requests.org/en/latest/) is a requirement.")
+
+import pathlib
+import hashlib
+import urllib2
+import urlparse
+import subprocess
+
+# Detecting Python 3 for version-dependent implementations
+PY3 = sys.version_info >= (3, 0)
+
+if not PY3:
+    raise Exception("We do not support Python 2 anymore.")
+
+# Checking whether it isn't Linux...
+if platform.system() != 'Linux':
+   print("ERROR: Unsupported OS...")
+   sys.exit(9)
+
+# Checking for sudo's' existance....
+proc = subprocess.Popen([
+    "/usr/bin/env", "command", "-p", "sudo"
+], stdout=subprocess.PIPE).stdout.read()
+
+if 'command not found' in proc.stdout.read():
+   print("ERROR: Sudo command doesn't exist!'")
+   sys.exit(9)
+
+###
+___dir = os.path.dirname(os.path.realpath(__file__))
 
 def usage():
     print("Usage: " + sys.argv[0] + " <arg1> <arg2> <arg3>")
