0dca06f817a410f8980c6f21eaeffce80cf32af7
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2022-05-23 09:07:30 +0200

    -

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#
+#  Copyright 2022 John Magdy Lotfy Kamel (Zorono) <johnmagdy437@yahoo.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# Last Edit: 2022/05/20 13:51:37
+#
 
 try:
     import time
@@ -40,7 +63,7 @@ try:
 except ImportError:
     raise ImportError("built-in 'hashlib' library is a requirement.")
 
-try: 
+try:
     import urllib
 except ImportError:
     raise ImportError("built-in 'urllib' library is a requirement.")
@@ -57,9 +80,9 @@ except ImportError:
 
 # Detecting Python 3 for version-dependent implementations
 if sys.version_info.major < 3:
-    raise Exception("Python's' major versions earlier than 3 is not supported!")
+    raise Exception("Python's major versions earlier than 3 is not supported!")
 
-cooldown()
+#cooldown()
 
 # Checking whether it isn't Linux...
 if platform.uname().system != 'Linux':
@@ -81,13 +104,16 @@ TMP_PATH = os.path.expanduser('~' + os.sep + '.tmp')
 def usage():
     print("Usage: " + sys.argv[0] + " <arg1> <arg2> <arg3>")
 
-def main():
-    print(len(sys.argv))
-    if len(sys.argv) != 4:
+def main(args):
+    print(len(args))
+    if len(args) != 4:
         usage()
+        return 0
     else:
-        parse(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-        time.sleep(1)	
-
+        parse(args[1], int(args[2]), int(args[3]))
+        time.sleep(1)
+        return 1
+sudo -Kk
 if __name__ == '__main__':
-    main()
+    sys.exit(main(sys.argv))
+
